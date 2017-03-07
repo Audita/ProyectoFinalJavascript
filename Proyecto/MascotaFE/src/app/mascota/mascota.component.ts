2bef293623fd83e72f32852d3ed1973df38d4fb0
Christian Castillo <christian.castillo@epn.edu.ec>
2017-03-07 14:18:24 -0500

    mascota creacion listar

--- Proyecto/MascotaFE/src/app/mascota/mascota.component.ts
+++ Proyecto/MascotaFE/src/app/mascota/mascota.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {MasterURLService} from "../service/master-url.service";
-import {Http} from "@angular/http";
+import {Http, Response} from "@angular/http";
 import {NgForm} from "@angular/forms";
 
 @Component({
@@ -11,81 +11,33 @@ import {NgForm} from "@angular/forms";
 })
 export class MascotaComponent implements OnInit {
   title: string = "Ingresa tu información";
+  subtitle: string = "Listado de mascotas";
   nuevaMascota= {};
   mascotas = [];
+  generos=[{genero:"Macho"},{genero:"Hembra"}];
+  tipos=[{tipo:"Perro"},{tipo:"Gato"},{tipo:"otro"}];
+  tamanios=[{tipo:"pequeño"},{tipo:"mediano"},{tipo:"grande"}];
   disabledButtons = {
     NuevaMascotaFormSumitButton: false
-  };
-  activeColor: string = 'green';
-  baseColor: string = '#ccc';
-  overlayColor: string = 'rgba(255,255,255,0.5)';
-
-  dragging: boolean = false;
-  loaded: boolean = false;
-  imageLoaded: boolean = false;
-  imageSrc: string = '';
-
-  handleDragEnter() {
-    this.dragging = true;
-  }
-
-  handleDragLeave() {
-    this.dragging = false;
-  }
-
-  handleDrop(e) {
-    e.preventDefault();
-    this.dragging = false;
-    this.handleInputChange(e);
-  }
-
-  handleImageLoad() {
-    this.imageLoaded = true;
-  //  this.iconColor = this.overlayColor;
-  }
-
-  handleInputChange(e) {
-    let file = e.dataTransfer ? e.dataTransfer.files[0] : e.target.files[0];
-
-    let pattern = /image-*/;
-    let reader = new FileReader();
-
-    if (!file.type.match(pattern)) {
-      alert('invalid format');
-      return;
-    }
-
-    this.loaded = false;
-
-    reader.onload = this._handleReaderLoaded.bind(this);
-    reader.readAsDataURL(file);
-  }
-
-  _handleReaderLoaded(e) {
-    let reader = e.target;
-    this.imageSrc = reader.result;
-    this.loaded = true;
-  }
-
-  _setActive() {
-   // this.borderColor = this.activeColor;
-    if (this.imageSrc.length === 0) {
-    //  this.iconColor = this.activeColor;
-    }
-
-//  _setInactive() {
-  //  this.borderColor = this.baseColor;
-  //  if (this.imageSrc.length === 0) {
-    //  this.iconColor = this.baseColor;
-   // }
-  //}
-
   };
 
   constructor(private _http: Http,
               private  _masterURL: MasterURLService) { }
 
   ngOnInit() {
+    this._http.get(this._masterURL.url + "Mascota")
+      .subscribe(
+        (res: Response) => {
+          this.mascotas = res.json()
+            .map((value) => {
+              value.formularioCerrado = true;
+              return value;
+            });
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
   }
   crearMascota(formulario: NgForm) {
     console.log("No se que va a imprimir: esperemso al body");
@@ -97,7 +49,8 @@ export class MascotaComponent implements OnInit {
       foto: formulario.value.foto,
       tipo: formulario.value.tipo,
       genero: formulario.value.genero,
-      tamanio: formulario.value.tamanio
+      estado:"activo",
+      tamanio: formulario.value.tamanio,
     }).subscribe(
       (res) => {
         console.log("No existieron errores");
